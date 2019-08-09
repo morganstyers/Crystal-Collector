1d35cdee07e5cdb698c7130ecb8ec3d564f602fd
Morgan Styers <morganstyers@gmail.com>
2019-08-09 15:59:31 -0400

    modify

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,26 +7,43 @@ magicNumber= Math.floor(Math.random()* (120 - 19)) + 19;
 $("#magicNum").text(magicNumber);
 /////////////////////////////////////
 
-
-var value= [];
+//4 ran integers
+var crystalValue= [];
 for (var i = 0; i < 4; i++) {
-value.push(Math.floor(Math.random() * 13)+1);
+crystalValue.push(Math.floor(Math.random() * 13)+1);
 }
-var str = "";
-for (var i = 0; i < 5; i++)
-{
-  str += "," + value[i];
-}
+console.log(crystalValue)
 
+var crystalOne= $("<img>");
+crystalOne.addClass("crystal-image");
+crystalOne.attr("src", "assets/images/gem-transparent-tumblr.png");
+crystalOne.attr("data-crystal-val", crystalValue[0]);
+$("#crystals").append(crystalOne);
+console.log(crystalValue)
 
-console.log(value)
+var crystalTwo=$("<img>");
+crystalTwo.addClass("crystal-image");
+crystalTwo.attr("src","assets/images/transparent-mineral-5.png");
+crystalTwo.attr("data-crystal-val", crystalValue[1])
+$("#crystals").append(crystalTwo);
 
+var crystalThree=$("<img>");
+crystalThree.addClass("crystal-image");
+crystalThree.attr("src","assets/images/gem-transparent-tumblr-1.png");
+crystalThree.attr("data-crystal-val", crystalValue[2])
+$("#crystals").append(crystalThree);
 
-}
+var crystalFour=$("<img>");
+crystalFour.addClass("crystal-image");
+crystalFour.attr("src","assets/images/drawing-gemstones-mineral-4.png");
+crystalFour.attr("data-crystal-val", crystalValue[3]);
+$("#crystals").append(crystalFour);
 
+$(".crystal-image").on("click",function(){
+    var crystalValue = ($(this).attr("data-crystal-val"));
+    crystalValue = parseInt(crystalValue);
+    console.log(crystalValue)
 
- 
-
-
-)
+})
+});
 
